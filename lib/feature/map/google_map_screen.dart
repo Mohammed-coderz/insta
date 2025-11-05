8615182e9f571587608cb2044aa153e5b6a257e6
Mohammed-coderz <m.AbuElhawa@coderz-tech.com>
2025-11-05 18:15:27 +0300

    feat: add url_launcher and basic map screen
    Adds the `url_launcher` package to handle opening external URLs.
    
    - Implements a new `GoogleMapScreen` which displays a Google Map centered on a specific location.
    - Adds an "go to" button on the map screen to launch Google Maps externally, though the URL is currently hardcoded and incomplete.
    - Updates native plugin registration files for Windows, Linux, and macOS to include `url_launcher`.

--- lib/feature/map/google_map_screen.dart
+++ lib/feature/map/google_map_screen.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+
+final Uri _url = Uri.parse('https://www.google.com/maps?q=$,<LNG>');
+Future<void> _launchUrl() async {
+  if (!await launchUrl(_url)) {
+    throw Exception('Could not launch $_url');
+  }
+}
 class GoogleMapScreen extends StatelessWidget {
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(31.9885700, 35.8757693),
+    zoom: 20,
+  );
+
+
   const GoogleMapScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text('Google Map')),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 500,
+            width: 400,
+            child: GoogleMap(
+                initialCameraPosition: _kGooglePlex,
+                mapType:MapType.satellite,
+
+
+            ),
+          ),
+          ElevatedButton(onPressed: _launchUrl, child: Text("go to"))
+        ],
+      ),
+    );
   }
 }
